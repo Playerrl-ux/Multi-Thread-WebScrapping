3ac780aacd8db0954fded68199f8a0c8514b9334
Playerrl-ux <luedybatista96@gmail.com>
2025-08-12 15:58:35 -0300

    refactor: Create the URLType and ADD it on the FilterAddress

---
+++ src/main/java/com/web/enums/URLType.java
@@ -0,0 +1,6 @@
+package com.web.enums;
+
+public enum URLType {
+    ERROR,
+    NORMAL
+}
--- src/main/java/com/web/filter/context/FilterAddress.java
+++ src/main/java/com/web/filter/context/FilterAddress.java
@@ -1,7 +1,9 @@
 package com.web.filter.context;
 
+import com.web.enums.URLType;
+
 import java.net.URI;
 import java.util.List;
 
-public record FilterAddress(URI uri, List<String> filteredLines) {
+public record FilterAddress(URI uri, List<String> filteredLines, URLType type) {
 }
