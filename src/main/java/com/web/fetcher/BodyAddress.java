d8c6dc8c8d11a21f23752b7ae0b49f9d90068a3a
Playerrl-ux <luedybatista96@gmail.com>
2025-08-12 20:29:02 -0300

    refactor: move fetcher classes

--- src/main/java/com/web/fetcher/BodyAddress.java
+++
@@ -1,5 +0,0 @@
-package com.web.fetcher;
-
-import java.net.URI;
-
-public record BodyAddress(URI uri, String html) {}
--- src/main/java/com/web/fetcher/IWebFetcher.java
+++
@@ -1,6 +0,0 @@
-package com.web.fetcher;
-
-public interface IWebFetcher {
-
-    void fetchTarget();
-}
