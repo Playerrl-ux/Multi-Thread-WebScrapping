6cbf71ac3c0609361462862e5d8a60e861fdc662
Playerrl-ux <luedybatista96@gmail.com>
2025-08-12 16:00:02 -0300

    refactor: Changes the logic to choose the name of the file to write the request content

--- src/main/java/com/web/filter/context/FilterContext.java
+++ src/main/java/com/web/filter/context/FilterContext.java
@@ -1,73 +1,79 @@
 package com.web.filter.context;
 
-import com.web.fetcher.BodyAddress;
+import com.web.Pipe;
+import com.web.enums.URLType;
 import com.web.filter.strategy.FilterClass;
 import com.web.filter.strategy.IHtmlFilter;
+import com.web.webFetcher.valve.BodyAddress;
 
 import java.lang.reflect.InvocationTargetException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.BlockingQueue;
 
-public class FilterContext implements Runnable{
+public class FilterContext implements Runnable, Pipe<BodyAddress> {
 
     private final Map<String, Class<? extends IHtmlFilter>> clazzMap;
     private final Map<String, IHtmlFilter> filterMap;
     private final BlockingQueue<BodyAddress> bodyAddressQueue;
-    private final BlockingQueue<FilterAddress> filterAddressQueue;
+    private final Pipe<FilterAddress> pipe;
 
     public FilterContext(BlockingQueue<BodyAddress> bodyAddressQueue,
-                         BlockingQueue<FilterAddress> filterAddressQueue) {
+                         Pipe<FilterAddress> pipe) {
         this.bodyAddressQueue = bodyAddressQueue;
-        this.filterAddressQueue = filterAddressQueue;
+        this.pipe = pipe;
         clazzMap = new HashMap<>();
-        for(var filter: FilterClass.values()){
+        for (var filter : FilterClass.values()) {
             clazzMap.put(filter.domain, filter.filterClazz);
         }
         filterMap = new HashMap<>();
     }
 
     public void run() {
-        try{
-            while(true) {
+        try {
+            while (true) {
                 try {
                     var bodyAddress = bodyAddressQueue.take();
-                    if(bodyAddress.uri() == null || bodyAddress.html() == null){
+                    if (bodyAddress.uri() == null || bodyAddress.html() == null) {
                         break;
                     }
-                    String domain = bodyAddress.uri().toString().split("/")[2];
+
+                    //mudar para somente .getHost depois dos testes
+                    String domain = new URI(bodyAddress.uri().getQuery().substring(5)).getHost();
 
                     IHtmlFilter filter = filterMap.get(domain);
                     if (filter != null) {
-                        filterAddressQueue.put(new FilterAddress(bodyAddress.uri(), filter.filter(bodyAddress.html())));
+                        pipe.put(new FilterAddress(bodyAddress.uri(), filter.filter(bodyAddress.html()),
+                                bodyAddress.type()));
                         continue;
                     }
 
                     var clazz = clazzMap.get(domain);
                     if (clazz == null) {
-                        throw new ClassNotFoundException("No filter class found for domain " + domain);
+                        pipe.put(new FilterAddress(bodyAddress.uri(), null, URLType.ERROR));
+                        continue;
                     }
                     filter = clazz.getDeclaredConstructor().newInstance();
                     filterMap.put(domain, filter);
-                    filterAddressQueue.put(new FilterAddress(bodyAddress.uri(), filter.filter(bodyAddress.html())));
+                    pipe.put(new FilterAddress(bodyAddress.uri(), filter.filter(bodyAddress.html()),
+                            bodyAddress.type()));
 
-                } catch (InstantiationException | IllegalAccessException | InvocationTargetException |
-                         NoSuchMethodException e) {
-                    throw new RuntimeException(e);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                } catch (ClassNotFoundException e) {
+                } catch (RuntimeException | URISyntaxException | IllegalAccessException | NoSuchMethodException |
+                         InstantiationException ignored) {
+                } catch (InvocationTargetException e) {
                     throw new RuntimeException(e);
                 }
             }
-
-        }finally {
-            while(true){
-                try{
-                    filterAddressQueue.put(new FilterAddress(null, null));
-                    break;
-                }catch (InterruptedException ignored){}
-            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            pipe.putPersitent(new FilterAddress(null, null, null));
         }
+    }
+
+    public void put(BodyAddress bodyAddress) throws InterruptedException {
+        bodyAddressQueue.put(bodyAddress);
     }
 }
